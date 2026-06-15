89c1b15d4827de48577acc635a9150f95a8e2a7b
sayem111103 <sayemul03@gmail.com>
2026-06-15 21:24:28 +0600

    Table schema completed

--- QUERY.sql
+++ QUERY.sql
@@ -44,13 +44,13 @@ CREATE TABLE Matches (
 -- 3. CREATE BOOKINGS TABLE
 -- =========================================================================
 CREATE TABLE Bookings (
-    booking_id TYPE,
-    user_id TYPE,
-    match_id TYPE,
-    seat_number TYPE,
-    payment_status TYPE,
-    total_cost TYPE,
-
+    booking_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES Users(user_id) NOT NULL,
+    match_id INT REFERENCES Matches(match_id) NOT NULL,
+    seat_number VARCHAR(20),
+    payment_status VARCHAR(20) CHECK(payment_status IN('Pending', 'Confirmed', 'Cancelled', 'Refunded')),
+    total_cost DECIMAL(10, 2) CHECK(total_cost >= 0),
+    CONSTRAINT unique1_match_seat UNIQUE(match_id, seat_number)
 -- Write your constraint to make 'booking_id' the Primary Key
 -- Write your Foreign Key constraint linking 'user_id' to the Users table
 -- Write your Foreign Key constraint linking 'match_id' to the Matches table
@@ -62,138 +62,34 @@ CREATE TABLE Bookings (
 -- DATA SEEDING: INSERT SAMPLE DATA INTO USERS
 -- =========================================================================
 INSERT INTO
-    Users (
-        user_id,
-        full_name,
-        email,
-        role,
-        phone_number
-    )
-VALUES (
-        1,
-        'Tanvir Rahman',
-        'tanvir@mail.com',
-        'Football Fan',
-        '+8801711111111'
-    ),
-    (
-        2,
-        'Asif Haque',
-        'asif@mail.com',
-        'Football Fan',
-        '+8801722222222'
-    ),
-    (
-        3,
-        'Sajjad Rahman',
-        'sajjad@mail.com',
-        'Ticket Manager',
-        '+8801733333333'
-    ),
-    (
-        4,
-        'Jannat Ara',
-        'jannat@mail.com',
-        'Football Fan',
-        NULL
-    );
+    Users ( user_id, full_name, email, role, phone_number)
+VALUES 
+    (1,'Tanvir Rahman','tanvir@mail.com','Football Fan','+8801711111111'),
+    (2,'Asif Haque','asif@mail.com','Football Fan','+8801722222222'),
+    (3,'Sajjad Rahman','sajjad@mail.com','Ticket Manager','+8801733333333'),
+    (4,'Jannat Ara','jannat@mail.com','Football Fan',NULL);
 
 -- =========================================================================
 -- DATA SEEDING: INSERT SAMPLE DATA INTO MATCHES
 -- =========================================================================
 INSERT INTO
-    Matches (
-        match_id,
-        fixture,
-        tournament_category,
-        base_ticket_price,
-        match_status
-    )
-VALUES (
-        101,
-        'Real Madrid vs Barcelona',
-        'Champions League',
-        150.00,
-        'Available'
-    ),
-    (
-        102,
-        'Man City vs Liverpool',
-        'Premier League',
-        120.00,
-        'Selling Fast'
-    ),
-    (
-        103,
-        'Bayern Munich vs PSG',
-        'Champions League',
-        130.00,
-        'Available'
-    ),
-    (
-        104,
-        'AC Milan vs Inter Milan',
-        'Serie A',
-        90.00,
-        'Sold Out'
-    ),
-    (
-        105,
-        'Juventus vs Roma',
-        'Serie A',
-        80.00,
-        'Available'
-    );
+    Matches ( match_id, fixture, tournament_category, base_ticket_price, match_status)
+VALUES 
+       (101,'Real Madrid vs Barcelona','Champions League',150.00,'Available'),
+       (102,'Man City vs Liverpool','Premier League',120.00,'Selling'),
+       (103,'Bayern Munich vs PSG','Champions League',130.00,'Fast'),
+       (104,'AC Milan vs Inter Milan','Serie A',90.00,'Sold Out'),
+       (105,'Juventus vs Roma','Serie A',80.00,'Available');
 
 -- =========================================================================
 -- DATA SEEDING: INSERT SAMPLE DATA INTO BOOKINGS
 -- =========================================================================
 INSERT INTO
-    Bookings (
-        booking_id,
-        user_id,
-        match_id,
-        seat_number,
-        payment_status,
-        total_cost
-    )
-VALUES (
-        501,
-        1,
-        101,
-        'A-12',
-        'Confirmed',
-        150.00
-    ),
-    (
-        502,
-        1,
-        102,
-        'B-04',
-        'Confirmed',
-        120.00
-    ),
-    (
-        503,
-        2,
-        101,
-        'A-13',
-        'Confirmed',
-        150.00
-    ),
-    (
-        504,
-        2,
-        101,
-        NULL,
-        NULL,
-        150.00
-    ),
-    (
-        505,
-        3,
-        102,
-        'C-20',
-        'Pending',
-        120.00
-    );
+    Bookings ( booking_id, user_id, match_id, seat_number, payment_status, total_cost)
+VALUES 
+    (501,1,101,'A-12','Confirmed',150.00),
+    (502,1,102,'B-04','Confirmed',120.00),
+    (503,2,101,'A-13','Confirmed',150.00),
+    (504,2,101,NULL,NULL,150.00),
+    (505,3,102,'C-20','Pending',120.00);
+
